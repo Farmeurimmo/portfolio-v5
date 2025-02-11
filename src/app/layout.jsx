aa66520a460c8b93a0de49bab729c0662c1efa7b
Farmeurimmo <contact@farmeurimmo.fr>
2025-02-11 19:37:54 +0100

    feat: add global layout.jsx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -5,7 +5,7 @@ import React from 'react';
 import {Navbar} from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import {Geist, Geist_Mono} from "next/font/google";
-import "./globals.css";
+import "./[locale]/globals.css";
 import ClientThemeProvider from "@/app/[locale]/ClientThemeProvider";
 
 const geistSans = Geist({
