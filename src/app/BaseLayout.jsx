b2646a0629be6b158be66c0510e873dabd14fc97
Farmeurimmo <contact@farmeurimmo.fr>
2025-03-06 19:16:21 +0100

    feat: new seo titles and desc + auto slicers

--- src/app/BaseLayout.jsx
+++ src/app/BaseLayout.jsx
@@ -18,7 +18,7 @@ export default function BaseLayout({children, locale = "en"}) {
             <meta name={"favicon"} content="https://cdn.farmeurimmo.fr/img/logo.png"/>
             <link rel={"canonical"} href={"https://farmeurimmo.fr"}/>
             <link rel="icon" href="https://cdn.farmeurimmo.fr/img/logo.jpg"/>
-            <link rel={"shortcut icon"} href={"https://cdn.farmeurimmo.fr/img/logo.jpg"} type={"image/x-icon"} />
+            <link rel={"shortcut icon"} href={"https://cdn.farmeurimmo.fr/img/logo.jpg"} type={"image/x-icon"}/>
 
             <link rel={"alternate"} hrefLang={"fr"} href={"https://farmeurimmo.fr/fr"}/>
             <link rel={"alternate"} hrefLang={"en"} href={"https://farmeurimmo.fr/en"}/>
--- src/app/[locale]/layout.jsx
+++ src/app/[locale]/layout.jsx
@@ -21,7 +21,7 @@ export const viewport = {
 }
 
 export async function generateMetadata({params}) {
-    const { locale } = await params;
+    const {locale} = await params;
     const headersList = await headers();
     let pathname = headersList.get("x-request-url")
         .replace("https://farmeurimmo.fr", "")
@@ -36,7 +36,7 @@ export async function generateMetadata({params}) {
     const metadata = {
         en: {
             default: {
-                title: "Farmeurimmo | Dev & SysAdmin Portfolio",
+                title: "Farmeurimmo | Developer and System Administrator",
                 description: "Discover my portfolio as a developer and system administrator. SaaS services, infrastructure management, and digital solutions.",
                 keywords: "developer, system administrator, SaaS, infrastructure management, programming, automation, cloud solutions, security, digital solutions",
                 "og:image": "https://cdn.farmeurimmo.fr/img/logo.png",
@@ -44,7 +44,7 @@ export async function generateMetadata({params}) {
                 "robots": "index, follow"
             },
             blog: {
-                title: "Farmeurimmo | Blog - Tech & Development",
+                title: "Farmeurimmo | Technology and Development Blog",
                 description: "Insights and discussions on technology, software development, system administration, and digital solutions.",
                 keywords: "blog, technology, development, system administration, programming, cloud computing, automation, software engineering, cybersecurity, digital solutions",
                 "og:image": "https://cdn.farmeurimmo.fr/img/logo.png",
@@ -52,7 +52,7 @@ export async function generateMetadata({params}) {
                 "robots": "index, follow"
             },
             projects: {
-                title: "Farmeurimmo | Dev & SysAdmin Projects",
+                title: "Farmeurimmo | Projects Developed and Ongoing",
                 description: "A showcase of my completed and ongoing projects in development, system administration, and digital solutions.",
                 keywords: "projects, software development, system administration, digital solutions, programming, automation, cloud solutions, infrastructure, DevOps, SaaS",
                 "og:image": "https://cdn.farmeurimmo.fr/img/logo.png",
@@ -60,9 +60,9 @@ export async function generateMetadata({params}) {
                 "robots": "index, follow"
             },
             legals: {
-                title: "Farmeurimmo | Legal Information",
-                description: "Legal information and terms of use for my portfolio website. (https://farmeurimmo.fr)",
-                keywords: "legal mentions, terms of use, privacy policy, data protection, cookies, website terms",
+                title: "Farmeurimmo | Terms & Legal Information",
+                description: "Find all legal information, terms of use, and privacy policies related to my portfolio website. Compliance and transparency.",
+                keywords: "legal, terms, privacy policy, data protection, cookies, website terms",
                 "og:image": "https://cdn.farmeurimmo.fr/img/logo.png",
                 "twitter:image": "https://cdn.farmeurimmo.fr/img/logo.png",
                 "robots": "index, nofollow"
@@ -70,7 +70,7 @@ export async function generateMetadata({params}) {
         },
         fr: {
             default: {
-                title: "Farmeurimmo | Portfolio Dev & Admin Sys",
+                title: "Farmeurimmo | Développeur et Administrateur Système",
                 description: "Découvrez mon portfolio de développeur et administrateur système. Services en SaaS, gestion d’infrastructure et solutions numériques.",
                 keywords: "développeur, administrateur système, SaaS, gestion des infrastructures, programmation, automatisation, solutions cloud, sécurité, solutions numériques",
                 "og:image": "https://cdn.farmeurimmo.fr/img/logo.png",
@@ -78,7 +78,7 @@ export async function generateMetadata({params}) {
                 "robots": "index, follow"
             },
             blog: {
-                title: "Farmeurimmo | Blog - Tech & Développement",
+                title: "Farmeurimmo | Blog sur la Technologie et le Développement",
                 description: "Réflexions et discussions sur la technologie, le développement, l'administration système et les solutions numériques.",
                 keywords: "blog, technologie, développement, administration système, programmation, cloud computing, automatisation, ingénierie logicielle, cybersécurité, solutions numériques",
                 "og:image": "https://cdn.farmeurimmo.fr/img/logo.png",
@@ -86,7 +86,7 @@ export async function generateMetadata({params}) {
                 "robots": "index, follow"
             },
             projects: {
-                title: "Farmeurimmo | Projets Dev & Admin Sys",
+                title: "Farmeurimmo | Projets développés et en cours",
                 description: "Une vitrine de mes projets terminés et en cours en développement, administration système et solutions numériques.",
                 keywords: "projets, développement logiciel, administration système, solutions numériques, programmation, automatisation, solutions cloud, infrastructure, DevOps, SaaS",
                 "og:image": "https://cdn.farmeurimmo.fr/img/logo.png",
@@ -94,8 +94,8 @@ export async function generateMetadata({params}) {
                 "robots": "index, follow"
             },
             legals: {
-                title: "Farmeurimmo | Mentions Légales",
-                description: "Informations légales et conditions d'utilisation pour mon site web portfolio. (https://farmeurimmo.fr)",
+                title: "Farmeurimmo | Conditions & Mentions Légales",
+                description: "Retrouvez toutes les mentions légales, conditions d’utilisation et politiques de confidentialité liées à mon site portfolio.",
                 keywords: "mentions légales, conditions d'utilisation, politique de confidentialité, protection des données, cookies, conditions du site web",
                 "og:image": "https://cdn.farmeurimmo.fr/img/logo.png",
                 "twitter:image": "https://cdn.farmeurimmo.fr/img/logo.png",
@@ -114,22 +114,24 @@ export async function generateMetadata({params}) {
             return;
         }
 
+        const truncate = (text, maxLength) => text.length > maxLength ? text.slice(0, maxLength - 3) + "..." : text;
+
         return {
-            title: meta.title,
-            description: meta.description,
+            title: truncate(meta.title, 60),
+            description: truncate(meta.description, 160),
             keywords: meta.keywords,
             robots: meta.robots,
             openGraph: {
-                title: meta.title,
-                description: meta.description,
-                images: [{ url: meta["og:image"] }],
+                title: truncate(meta.title, 60),
+                description: truncate(meta.description, 160),
+                images: [{url: meta["og:image"]}],
                 url: `https://farmeurimmo.fr/${locale}${pathname}`,
                 siteName: "Farmeurimmo",
             },
             twitter: {
-                title: meta.title,
-                description: meta.description,
-                images: [{ url: meta["twitter:image"] }],
+                title: truncate(meta.title, 60),
+                description: truncate(meta.description, 160),
+                images: [{url: meta["twitter:image"]}],
             },
         };
     } catch (e) {
