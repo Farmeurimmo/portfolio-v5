967994100e1efd64afed1f0269a86a3aa6619c7f
Farmeurimmo <contact@farmeurimmo.fr>
2025-03-08 11:49:51 +0100

    Revert "refacto: moved CORS to middleware"
    This reverts commit 4c78f8c02eb65bb5a73eb82db8c133b787f0cb73.

--- src/middleware.js
+++ src/middleware.js
@@ -12,17 +12,7 @@ export default async function customMiddleware(req, ev) {
 
     if (pathname.startsWith('/_next') || pathname.startsWith('/api') || pathname.startsWith("/installHook.js") ||
         pathname.startsWith("/sitemap.xml") || pathname.startsWith("/robots.txt")) {
-        const res = NextResponse.next()
-
-        if (pathname.startsWith(("/api"))) {
-            const origin = req.headers.get("Origin");
-            const allowedOrigin = origin && (origin.endsWith(".farmeurimmo.fr") || origin === "https://farmeurimmo.fr") ? origin : "null";
-
-            res.headers.append('Access-Control-Allow-Origin', allowedOrigin);
-            res.headers.append('Access-Control-Allow-Methods', 'POST');
-            res.headers.append('Access-Control-Allow-Headers', 'Content-Type');
-        }
-        return res;
+        return NextResponse.next();
     }
 
     if (!/^\/(fr|en)/.test(pathname)) {
