c2c18d3a77de85def0790ff1286ef4833c5a786b
Farmeurimmo <contact@farmeurimmo.fr>
2025-03-08 11:55:37 +0100

    fix: webhook message

--- src/app/api/contact/route.js
+++ src/app/api/contact/route.js
@@ -7,12 +7,6 @@ export async function POST(request) {
 
     const body = await request.json();
     const {email, username, object, message} = body;
-    const data = {
-        email,
-        username,
-        object,
-        message
-    };
 
     try {
         const response = await fetch(webhook, {
@@ -20,13 +14,17 @@ export async function POST(request) {
             headers: {
                 "Content-Type": "application/json"
             },
-            body: JSON.stringify(data)
+            body: JSON.stringify({
+                'content': `**Username:** ${username}\n**Email:** ${email}\n**Object:** ${object}\n**Message:** ${message}`,
+                'username': 'Contact Form (v5)',
+                'avatar_url': 'https://cdn.farmeurimmo.fr/img/logo.png'
+            })
         });
 
         if (response.ok) {
             return new Response("Message sent", {status: 200});
         } else {
-            return new Response("The contact url did not like the data.", {status: 500});
+            return new Response("The contact url did not like the data. Code " + response.status, {status: 500});
         }
     } catch (error) {
         return new Response("An error occurred while sending the message.", {status: 500});
