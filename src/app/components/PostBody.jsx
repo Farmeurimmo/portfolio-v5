dc96c8bfb69f500bb62dbdca28a75c60c55374e5
Farmeurimmo <contact@farmeurimmo.fr>
2025-03-08 10:03:38 +0100

    fix: blog css errors

--- src/app/components/PostBody.jsx
+++ src/app/components/PostBody.jsx
@@ -1,10 +1,8 @@
-import markdownStyles from "./markdown-styles.module.css";
-
 export function PostBody({content}) {
     return (
         <div className="max-w-3xl mx-auto mb-8">
             <div
-                className={markdownStyles["markdown"]}
+                className={"markdown"}
                 dangerouslySetInnerHTML={{__html: content}}
             />
         </div>
