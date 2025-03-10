dd78788f05d01d10f18d184d654e85b33c7a3a54
Farmeurimmo <contact@farmeurimmo.fr>
2025-03-10 13:57:20 +0100

    fix: increase max width of PostBody component

--- src/app/components/PostBody.jsx
+++ src/app/components/PostBody.jsx
@@ -1,6 +1,6 @@
 export function PostBody({content}) {
     return (
-        <div className="max-w-3xl mx-auto mb-8">
+        <div className="max-w-5xl mx-auto mb-8">
             <div
                 className={"markdown"}
                 dangerouslySetInnerHTML={{__html: content}}
