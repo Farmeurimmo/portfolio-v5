6f61a7ee05285ed136dd5a046329fc218d270552
Farmeurimmo <contact@farmeurimmo.fr>
2025-05-22 22:14:06 +0200

    fix: sitemap generation for projects

--- src/lib/projects.js
+++ src/lib/projects.js
@@ -3,7 +3,7 @@ import {join} from "path";
 
 export function getAllProjectSlugs() {
     const locale = "fr";
-    const projectsDir = join(process.cwd(), "src/app", locale, "projects");
+    const projectsDir = join(process.cwd(), "src/app/[locale]/projects");
     return fs.readdirSync(projectsDir).filter((name) =>
         fs.statSync(join(projectsDir, name)).isDirectory()
     );
