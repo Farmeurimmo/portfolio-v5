f36890242b9d7c0bb1d04cbd21c854c4bf8e76c0
Farmeurimmo <contact@farmeurimmo.fr>
2025-12-12 10:14:11 +0100

    fix: add padding and overflow scroll to PostBody component

--- src/app/components/PostBody.jsx
+++ src/app/components/PostBody.jsx
@@ -1,6 +1,6 @@
 export function PostBody({content}) {
     return (
-        <div className="max-w-5xl mx-auto mb-8">
+        <div className="max-w-5xl mx-auto mb-8 px-4 overflow-scroll">
             <div
                 className={"markdown"}
                 dangerouslySetInnerHTML={{__html: content}}
