faf55aae99688e7b9dc9dd1a4b03b7600da77920
Farmeurimmo <contact@farmeurimmo.fr>
2025-12-16 21:54:57 +0100

    refactoring: switched from domains to remotePatterns (was deprecated)

--- next.config.mjs
+++ next.config.mjs
@@ -5,17 +5,47 @@ const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: [
-            'cdn.farmeurimmo.fr',
-            'static.spigotmc.org',
-            'skillicons.dev',
-            'junit.org',
-            'img.daisyui.com',
-            'downloads.portainer.io',
-            'www.influxdata.com',
-            'raw.githubusercontent.com',
-            'assets.zabbix.com',
-            'github.com',
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'cdn.farmeurimmo.fr',
+            },
+            {
+                protocol: 'https',
+                hostname: 'static.spigotmc.org',
+            },
+            {
+                protocol: 'https',
+                hostname: 'skillicons.dev',
+            },
+            {
+                protocol: 'https',
+                hostname: 'junit.org',
+            },
+            {
+                protocol: 'https',
+                hostname: 'img.daisyui.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'downloads.portainer.io',
+            },
+            {
+                protocol: 'https',
+                hostname: 'www.influxdata.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'raw.githubusercontent.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'assets.zabbix.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'github.com',
+            },
         ],
         dangerouslyAllowSVG: true,
         contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
