b8551f77951aa9e599189b7afc23e0f3533cfdd8
Farmeurimmo <contact@farmeurimmo.fr>
2026-06-05 19:05:41 +0200

    feat: refactor markdown rendering to use ReactMarkdown component

--- src/app/components/PostBody.jsx
+++ src/app/components/PostBody.jsx
@@ -2,9 +2,9 @@ export function PostBody({content}) {
     return (
         <div className="max-w-5xl mx-auto mb-8 px-4 overflow-scroll">
             <div
-                className={"markdown"}
-                dangerouslySetInnerHTML={{__html: content}}
-            />
+                className={"markdown"}>
+                {content}
+            </div>
         </div>
     );
 }
--- src/lib/markdownToHtml.js
+++ src/lib/markdownToHtml.js
@@ -1,23 +1,14 @@
-import {unified} from 'unified'
-import remarkParse from "remark-parse";
 import remarkGfm from "remark-gfm";
-import remarkRehype from "remark-rehype";
-import rehypeStringify from "rehype-stringify";
 import rehypeHighlight from 'rehype-highlight'
-import rehypeHighlightCodeLines from 'rehype-highlight-code-lines'
-import remarkImages from 'remark-images'
- import rehypeRaw from "rehype-raw";
+import ReactMarkdown from "react-markdown";
 
 export default async function markdownToHtml(markdown) {
-    const result = await unified()
-        .use(remarkParse)
-        .use(remarkGfm)
-        .use(remarkRehype, {allowDangerousHtml: true})
-        .use(rehypeRaw)
-        .use(rehypeHighlight)
-        .use(rehypeHighlightCodeLines)
-        .use(rehypeStringify)
-        .use(remarkImages)
-        .process(markdown);
-    return result.toString();
+    return (
+        <ReactMarkdown
+            remarkPlugins={[remarkGfm]}
+            rehypePlugins={[rehypeHighlight]}
+        >
+            {markdown}
+        </ReactMarkdown>
+    );
 }
